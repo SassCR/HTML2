2639c099d2168b3eadbec8ae1281d708d440cfd3
SassCR <renefodao123@gmail.com>
2026-02-25 22:38:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function mudarMensagem(){
     var elemento = document.getElementById("mensagem")
-    elemento.textContent = "Você clicou no botão! Boa interatividade!"
+    elemento.textContent = "Olá mundo!"
 }
